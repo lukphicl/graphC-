740ab11ed51ec9240c551a17bb7814625db068cc
Luke Clover <lukphicl@umich.edu>
2018-04-05 16:35:27 -0400

    Initial Commit

---
+++ Project-7/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Project-7
+//
+//  Created by Luke Clover on 4/5/18.
+//  Copyright © 2018 Luke Clover. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
