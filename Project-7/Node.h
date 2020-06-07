671506193427ac597094998c6ccce4ad1a64e030
Luke Clover <lukphicl@umich.edu>
2020-06-07 19:22:05 -0400

    Practice for getting more proficient with git

--- Project-7/Node.h
+++ Project-7/Node.h
@@ -15,6 +15,7 @@ public:
     void put(ostream &out);                        // Displays node
     void connect(node *map, int dist);             // Connects one node to another
     void Depth(ostream &out);                      // Performs depth-first search
+    //practice new line bleh bleh blah
     void Breadth(ostream &out, queue<node> &list); // Performs breadth-first search
 private:
     string val;
